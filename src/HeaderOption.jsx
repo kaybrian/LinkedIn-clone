588b21d9cf431e7e55fdd8aef065d8a5114962a4
Kayongo brian <kayonbrian@gmail.com>
2021-12-29 13:11:01 +0300

    added the image icon on the navbar

--- src/HeaderOption.jsx
+++ src/HeaderOption.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './HeaderOption.css';
+import { Avatar } from '@mui/material';
 
-function HeaderOption({Icon, title}) {
+function HeaderOption({avatar, Icon, title}) {
     return (
         <div className="headerOption">
             {Icon && <Icon className='headerOption_icon' />}
+            {avatar && <Avatar src={avatar} className='headerOption_icon' />}
             <h5 className='headerOption_title'>{title}</h5>
         </div>
     )
